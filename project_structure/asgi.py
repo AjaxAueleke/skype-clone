6429e8931529725426062fcde7b678c061668165
AjaxAueleke <ahmed.jamil7410@gmail.com>
2023-05-07 21:33:02 +0500

    work complete

---
+++ project_structure/asgi.py
@@ -0,0 +1,21 @@
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_structure.settings')
+django_asgi_app = get_asgi_application()
+
+import messaging.routing
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                messaging.routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
